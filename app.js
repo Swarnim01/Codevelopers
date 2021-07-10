db5455bb231b319c987bd9fcd1a8fde8088a5cba
Swarnim Gupta <swarnimgupta2000@gmail.com>
2021-07-10 20:41:30 +0530

    testing

--- app.js
+++ app.js
@@ -42,13 +42,6 @@ app.use(
     credentials: true,
   })
 );
-
-if(process.env.NODE_ENV === 'production'){
-  app.use(express.static('client/build'));
-  app.get('*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  })
-}
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -66,6 +59,13 @@ app.use('/createpost',PostRouter);
 app.use('/showpost',ShowPostRouter);
 app.use('/myprofile',MyProfileRouter);
 app.use('/protected',ProtectedRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
