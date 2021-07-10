5ff271d27868b95273ce208b820ac7d8e82b7dbb
Swarnim Gupta <swarnimgupta2000@gmail.com>
2021-07-10 20:52:21 +0530

    testing

--- app.js
+++ app.js
@@ -62,9 +62,6 @@ app.use('/protected',ProtectedRouter);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
 }
 
 // catch 404 and forward to error handler
